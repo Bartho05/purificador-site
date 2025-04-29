50349bcd377f89cc42b64bd6c81a0b4a997c2408
Bartho05 <eliasbartholomeu17@gmail.com>
2025-04-28 22:43:09 -0300

    script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ faders.forEach(fader => {
 // Mostrar/ocultar botão de voltar ao topo
 window.onscroll = function() {
     const backToTop = document.getElementById("backToTop");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
         backToTop.style.display = "block";
     } else {
         backToTop.style.display = "none";
