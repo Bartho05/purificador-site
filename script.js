6b165f1edcbbf92f1016f2f677c669594e5f0cae
Bartho05 <eliasbartholomeu17@gmail.com>
2025-04-28 22:06:40 -0300

    asd

---
+++ script.js
@@ -0,0 +1,20 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 0.1,
+    rootMargin: "0px 0px -20px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('appear');
+        } else {
+            entry.target.classList.remove('appear');
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
